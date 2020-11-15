374191d4c399d451bfa539583d9722cdd17859f5
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-11-16 01:50:04 +0530

    added final structure need to add photos and labels

--- kavindu/src/app/home/home.component.ts
+++ kavindu/src/app/home/home.component.ts
@@ -12,4 +12,23 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  onClickLinkedin() {
+    window.location.href = 'https://www.linkedin.com/in/kavindu-gayantha-73478615b/';
+  }
+  onClickGithub() {
+    window.location.href = 'https://github.com/Kavindu-Gayantha';
+  }
+  onClickFacebook() {
+    window.location.href = 'https://www.facebook.com/profile.php?id=100008929308296';
+  }
+  onClickMedium() {
+    window.location.href = 'https://medium.com/@96kavindugayantha';
+  }
+  onClickYoutube() {
+    window.location.href = 'https://www.youtube.com/channel/UC6RVPaqYWHRwBDpl7RFRrdg?view_as=subscriber';
+  }
+  onClickStack() {
+    window.location.href = 'https://stackoverflow.com/users/9173526/kavindu-gayantha';
+  }
+
 }
