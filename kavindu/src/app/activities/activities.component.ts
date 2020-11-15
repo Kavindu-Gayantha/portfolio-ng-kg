7de0cf2cfb58145e64c54b4ce8c1444573e0293b
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-11-16 04:04:19 +0530

    contact form is ready can submit anything

--- kavindu/src/app/activities/activities.component.ts
+++ kavindu/src/app/activities/activities.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {ContactServiceService} from '../contact-service.service';
 
 @Component({
   selector: 'app-activities',
@@ -8,15 +9,37 @@ import { FormControl } from '@angular/forms';
 })
 export class ActivitiesComponent implements OnInit {
 
-  email= new FormControl();
-  name=new FormControl();
+  // email= new FormControl();
+  // name=new FormControl();
+  FormData: FormGroup;
 
-  constructor() { }
+  constructor(
+    private builder: FormBuilder,
+    private contact: ContactServiceService,
+    ) { }
 
   ngOnInit() {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+      Comment: new FormControl('', [Validators.required])});
   }
-  save(){
-    alert(`your email is ${this.email.value} and your name is ${this.name.value}`)
+  onSubmit(FormData) {
+    console.log(FormData);
+    // @ts-ignore
+    this.contact.postMsg(FormData)
+      .subscribe(res => {
+        location.href = 'https://mailthis.to/confirm';
+        console.log(res);
+      }, error => {
+          console.warn(error.resText);
+
+          console.log({error});
+        });
   }
+
+  // save(){
+  //   alert(`your email is ${this.email.value} and your name is ${this.name.value}`)
+  // }
 
 }
